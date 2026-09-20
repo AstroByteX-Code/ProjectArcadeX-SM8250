886a8b641cee9f589cefc8b53d18c1571235fb28
AstroByteX-Code <codebymianhamza@gmail.com>
2026-09-20 19:02:16 +0500

    wlan: refine fixed-Wifi patch

--- platform/sm_8250/vendor_patches/wlan/fixed-Wifi.sh
+++ platform/sm_8250/vendor_patches/wlan/fixed-Wifi.sh
@@ -10,14 +10,10 @@ LOG_BEGIN "Fixing Wi‑Fi..."
 
 # Remove old Wi‑Fi service binaries
 SILENT REMOVE "vendor" "bin/hw/wpa_supplicant"
-SILENT REMOVE "vendor" "bin/hw/vendor.samsung.hardware.wifi@2.0-service"
-SILENT REMOVE "vendor" "bin/hw/android.hardware.wifi@1.0-service"
 LOG_INFO "Removed outdated Wi‑Fi service binaries"
 
 # Inject stable Wi‑Fi blobs from y2q firmware
 ADD_FROM_FW "y2q" "vendor" "bin/hw/wpa_supplicant"
-ADD_FROM_FW "y2q" "vendor" "bin/hw/vendor.samsung.hardware.wifi@2.0-service"
-ADD_FROM_FW "y2q" "vendor" "bin/hw/android.hardware.wifi@1.0-service"
 LOG_INFO "Injected Wi‑Fi service binaries from y2q firmware"
 
 LOG_END "Wi‑Fi HAL fix applied successfully"
