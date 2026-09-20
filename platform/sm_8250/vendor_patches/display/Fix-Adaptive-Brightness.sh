0d4c47ca6d74c681d33628d531e02eb655a99a24
AstroByteX-Code <codebymianhamza@gmail.com>
2026-09-20 19:01:49 +0500

    display: update adaptive brightness patch

--- platform/sm_8250/vendor_patches/display/Fix-Adaptive-Brightness.sh
+++ platform/sm_8250/vendor_patches/display/Fix-Adaptive-Brightness.sh
@@ -11,13 +11,11 @@ LOG_BEGIN "Fixing Brightness HAL..."
 # Remove old light HAL/service stubs
 SILENT REMOVE "vendor" "bin/hw/vendor.samsung.hardware.light-service"
 SILENT REMOVE "vendor" "lib64/vendor.samsung.hardware.light-V1-ndk_platform.so"
-SILENT REMOVE "vendor" "lights.kona.so"
 LOG_INFO "Removed outdated light HAL and service binaries"
 
 # Inject stable light HAL blobs from y2q firmware
 ADD_FROM_FW "y2q" "vendor" "bin/hw/vendor.samsung.hardware.light-service"
 ADD_FROM_FW "y2q" "vendor" "lib64/vendor.samsung.hardware.light-V1-ndk_platform.so"
-ADD_FROM_FW "y2q" "vendor" "lib64/lights.kona.so"
 LOG_INFO "Injected light HAL and service binaries from y2q firmware"
 
 LOG_END "Brightness HAL fix applied successfully"
